c4e7d01d9b09e24ba6b4580bfd5a8b27fe0cd822
Chris <chriswu.ca@gmail.com>
2025-12-08 17:30:42 -0800

    Disable system theme preference in ThemeProvider.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
-          enableSystem
           disableTransitionOnChange
         >
           {children}
